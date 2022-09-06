7fce384a8d728447eb04db3804019143939ff272
Emilio Sandoval Palomino <80537193+milioe@users.noreply.github.com>
2022-09-06 16:31:47 -0500

    Add files via upload

--- utils.py
+++ utils.py
@@ -51,4 +51,39 @@ def plot_boundaries(model, X_train, y_train, X_test, y_test):
 
 
 
+def plot_boundaries_circles(model, X, y):
+  """
+  Imprime la toma de decisión del modelo
+  Esta función ha sido adaptada de
+   1. CS231n - https://cs231n.github.io/neural-networks-case-study/
+   2. Made with ML basics - https://github.com/GokuMohandas/MadeWithML/blob/main/notebooks/08_Neural_Networks.ipynb
+  """
+  # Define los ejes de decisión del plot y crea un meshgrid (un meshgrid recibe las matrices de coordenadas de los vectores de coordenadas)
+  x_min, x_max = X[:, 0].min() - 0.1, X[:, 0].max() + 0.1
+  y_min, y_max = X[:, 1].min() - 0.1, X[:, 1].max() + 0.1
+  xx, yy = np.meshgrid(np.linspace(x_min, x_max, 100),
+                       np.linspace(y_min, y_max, 100))
+  
+  # Crea los valores de X (Vamos a predecir sobre estos)
+  x_in = np.c_[xx.ravel(), yy.ravel()] # revisa si la dimensión final del output del modelo. is es mas grande a 1, es multiclase
+  
+  # Realizamos un reshape de las predicciones para poder graficarlas
+  y_pred = model.predict(x_in)
+
+  # Revisa si hay problemas de multiclase
+  if model.output_shape[-1] > 1: # revisa si la dimensión final del output del modelo. is es mas grande a 1, es multiclase
+    print("doing multiclass classification...")
+    # Reshape
+    y_pred = np.argmax(y_pred, axis=1).reshape(xx.shape)
+  else:
+    print("doing binary classifcation...")
+    y_pred = np.round(np.max(y_pred, axis=1)).reshape(xx.shape)
+  
+  # Imprime boundary
+  plt.contourf(xx, yy, y_pred, cmap=plt.cm.RdYlBu, alpha=0.7)
+  plt.scatter(X[:, 0], X[:, 1], c=y, s=40, cmap=plt.cm.RdYlBu)
+  plt.xlim(xx.min(), xx.max())
+  plt.ylim(yy.min(), yy.max())
+
+
   
