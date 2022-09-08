ea7023396b7a923ad7abcc9b98b5b61250debb00
Emilio Sandoval Palomino <80537193+milioe@users.noreply.github.com>
2022-09-08 16:32:35 -0500

    Add files via upload

--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import itertools
+from sklearn.metrics import confusion_matrix
 
 def plot_decision_boundary(model, X, y):
   """
@@ -127,5 +129,64 @@ def plot_loss_curves(history):
   plt.xlabel('Epochs')
   plt.legend();
 
+
+
+def make_confusion_matrix(y_true, y_pred, classes=None, figsize=(10, 10), text_size=15): 
+  """
+    Crea una matriz de confusion comparando las predicciones y las y_true
+
+  Si el argumento de clases se ha ocupado, la matriz de confusión se etiquetara, sino,
+  se devolverán n numeros correspondientes a n clases
+
+  Args:
+    y_true: Etiquetas reales
+    y_pred: Arreglo de etiquetas de predicción (tienen que ser del mismo shape que y_true)
+    classes: Arreglo con nombre de clases
+    figsize: Tamaño de la figura
+    text_size: Tamaó de texto
+  
+  Returns:
+    Plot de matriz de confusión
+
+
+  """  
+  # Crea matriz de confusión
+  cm = confusion_matrix(y_true, y_pred)
+  cm_norm = cm.astype("float") / cm.sum(axis=1)[:, np.newaxis] # normalize it
+  n_classes = cm.shape[0] # find the number of classes we're dealing with
+
+  # Plot 
+  fig, ax = plt.subplots(figsize=figsize)
+  cax = ax.matshow(cm, cmap=plt.cm.Blues)
+  fig.colorbar(cax)
+
+  # Si existe lista de clases
+  if classes:
+    labels = classes
+  else:
+    labels = np.arange(cm.shape[0])
+  
+  # Etiquetas de los ejes
+  ax.set(title="Confusion Matrix",
+         xlabel="Predicted label",
+         ylabel="True label",
+         xticks=np.arange(n_classes), # Creación de ejes
+         yticks=np.arange(n_classes), 
+         xticklabels=labels, 
+         yticklabels=labels)
+  
+  # Etiquetas del eje
+  ax.xaxis.set_label_position("bottom")
+  ax.xaxis.tick_bottom()
+
+  # Umbral de colores diferentes
+  threshold = (cm.max() + cm.min()) / 2.
+
+  # Texto de cada celda
+  for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+    plt.text(j, i, f"{cm[i, j]} ({cm_norm[i, j]*100:.1f}%)",
+             horizontalalignment="center",
+             color="white" if cm[i, j] > threshold else "black",
+             size=text_size)
 
   
